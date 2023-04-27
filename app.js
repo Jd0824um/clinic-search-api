93454cc5055ecc604c9ead96b757a906079d9611
John Zika <JohnZikaJr@outlook.com>
2023-04-27 17:14:46 -0500

    Adding docker support and clean up

--- app.js
+++ app.js
@@ -27,21 +27,22 @@ app.get("/api", (request, response) => {
       var searchResult = data;
 
       var name = request.query.name;
+      var state = request.query.state || request.query.stateCode; // Can use state or stateCode as a query parameter
+      var availabilityFrom = request.query.from;
+      var availabilityTo = request.query.to;
+
       if (name) {
         searchResult = searchResult.filter(clinic => clinic?.name === name || clinic?.clinicName === name);
       }
 
-      var state = request.query.state || request.query.stateCode; // Can use state or stateCode as a query parameter
       if (state) {
         searchResult = searchResult.filter(clinic => clinic?.stateName === state || clinic?.stateCode === state);
       }
 
-      var availabilityFrom = request.query.from;
       if (availabilityFrom) {
         searchResult = searchResult.filter(clinic => clinic?.availability?.from === availabilityFrom || clinic?.open?.from === availabilityFrom);
       }
 
-      var availabilityTo = request.query.to;
       if (availabilityTo) {
         searchResult = searchResult.filter(clinic => clinic?.availability?.to === availabilityTo || clinic?.open?.to === availabilityTo);
       }
